0a2a73815aaf4e8635d71f0e85d55a38efba0de6
Mohanraj111789 <kit27.am30.com>
2025-09-16 13:58:04 +0530

    db connected

--- src/main/java/com/todo/Main.java
+++ src/main/java/com/todo/Main.java
@@ -1,7 +1,15 @@
 package com.todo;
-
-public class Main {
-    
+import com.todo.util.DatabaseConnection;
+import java.sql.Connection;
+    public class Main {
     public static void main(String[] args) {
+        DatabaseConnection db_Connection = new DatabaseConnection();
+        try{
+            Connection cn = db_Connection.getDBConnection();
+            System.out.println("✅Database Connection Successful");
+        }
+        catch(Exception e){
+            System.out.println("❌Database Connection Failed: " + e.getMessage());
+        }
     }
 }
---
+++ src/main/java/com/todo/util/DatabaseConnection.java
@@ -0,0 +1,27 @@
+package com.todo.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+     public static final String URL = "jdbc:mysql://localhost:3306/todo";
+     public static final String USER = "root";
+     public static final String PASSWORD = "Kit23@12345";
+     static
+     {
+          try 
+          {
+               Class.forName("com.mysql.cj.jdbc.Driver");
+          } catch (ClassNotFoundException e) 
+          {
+               e.printStackTrace();
+          }
+     }
+     public Connection getDBConnection() throws SQLException
+     {
+          return DriverManager.getConnection(URL, USER, PASSWORD);
+     }
+      
+}
+
